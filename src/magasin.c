693e749b12ad7c3873760bf625732288ccaf85dc
Augaton <Mulleraurelien@hotmail.fr>
2026-01-06 14:50:37 +0100

    [Change] Catégorie dans le shop

--- src/magasin.c
+++ src/magasin.c
@@ -3,71 +3,67 @@
 #include <stdio.h>
 
 void ouvrirMagasin(Vaisseau *joueur) {
-    int choix = 0;
-    while (choix != 5) { // Option 5 pour quitter
+    int categorie = 0;
+    while (categorie != 4) { // 4 pour quitter le magasin
         effacerEcran();
         printf("--- STATION COMMERCIALE ---\n");
-        printf("Ferraille disponible : %d\n", joueur->ferraille);
-        printf("Etat du vaisseau : Coque [%d/%d] | Bouclier Max [%d] | Missiles [%d]\n", 
-                joueur->coque, joueur->coqueMax, joueur->bouclierMax, joueur->missiles);
+        printf("Ferraille : %d | Coque : %d/%d | Missiles : %d\n", 
+                joueur->ferraille, joueur->coque, joueur->coqueMax, joueur->missiles);
         printf("---------------------------\n");
-        printf("1. Reparer Coque (+5)       - 10 Ferrailles\n");
-        printf("2. Acheter Missiles (+3)    - 15 Ferrailles\n");
-        printf("3. Ameliorer Lasers (+1)    - 40 Ferrailles\n");
-        printf("4. Ameliorer Bouclier (+1)  - 50 Ferrailles\n");
-        printf("5. Quitter la station\n");
-        printf("\nVotre choix : ");
-        scanf("%d", &choix);
+        printf("1. [MAINTENANCE] (Reparations & Munitions)\n");
+        printf("2. [UPGRADES]    (Armes, Boucliers, Moteurs)\n");
+        printf("3. [SERVICES]    (Vendre du carburant, ect...)\n");
+        printf("4. QUITTER\n");
+        printf("\nChoisir categorie : ");
+        scanf("%d", &categorie);
 
-        switch(choix) {
-            case 1: // Réparation
-                if (joueur->ferraille >= 10 && joueur->coque < joueur->coqueMax) {
-                    joueur->ferraille -= 10;
-                    joueur->coque += 5;
-                    if (joueur->coque > joueur->coqueMax) joueur->coque = joueur->coqueMax;
-                    printf("Coque reparee !\n");
-                } else {
-                    printf("Action impossible (Ferraille insuffisante ou coque pleine).\n");
-                }
-                break;
+        if (categorie == 1) {
+            // --- SOUS-MENU MAINTENANCE ---
+            int choix = 0;
+            printf("\n-- MAINTENANCE --\n");
+            printf("1. Reparer Coque (+5)  - 10 Fer\n");
+            printf("2. Acheter Missiles (+3) - 15 Fer\n");
+            printf("3. Retour\n");
+            scanf("%d", &choix);
 
-            case 2: // Missiles
-                if (joueur->ferraille >= 15) {
-                    joueur->ferraille -= 15;
-                    joueur->missiles += 3;
-                    printf("Missiles charges !\n");
-                } else {
-                    printf("Pas assez de ferraille...\n");
-                }
-                break;
+            if (choix == 1 && joueur->ferraille >= 10 && joueur->coque < joueur->coqueMax) {
+                joueur->ferraille -= 10;
+                joueur->coque = (joueur->coque + 5 > joueur->coqueMax) ? joueur->coqueMax : joueur->coque + 5;
+                printf("Reparation OK.\n");
+            } else if (choix == 2 && joueur->ferraille >= 15) {
+                joueur->ferraille -= 15;
+                joueur->missiles += 3;
+                printf("Missiles recus.\n");
+            }
+        } 
+        else if (categorie == 2) {
+            // --- SOUS-MENU UPGRADES ---
+            int choix = 0;
+            printf("\n-- AMELIORATIONS --\n");
+            printf("1. Laser +1 (Actuel: %d)     - 40 Fer\n", joueur->armes);
+            printf("2. Bouclier +1 (Actuel: %d)  - 50 Fer\n", joueur->bouclierMax);
+            printf("3. Moteurs +1 (Esquive)      - 30 Fer\n");
+            printf("4. Retour\n");
+            scanf("%d", &choix);
 
-            case 3: // Lasers
-                if (joueur->ferraille >= 40) {
-                    joueur->ferraille -= 40;
-                    joueur->armes += 1;
-                    printf("Lasers ameliores ! Puissance : %d\n", joueur->armes);
-                } else {
-                    printf("Ferraille insuffisante.\n");
-                }
-                break;
-
-            case 4: // Bouclier Max
-                if (joueur->ferraille >= 50) {
-                    joueur->ferraille -= 50;
-                    joueur->bouclierMax += 1;
-                    printf("Capacite du bouclier augmentee : %d\n", joueur->bouclierMax);
-                } else {
-                    printf("Ferraille insuffisante.\n");
-                }
-                break;
-
-            case 5:
-                printf("Depart de la station...\n");
-                break;
-
-            default:
-                printf("Choix invalide.\n");
+            if (choix == 1 && joueur->ferraille >= 40) {
+                joueur->ferraille -= 40;
+                joueur->armes++;
+                printf("Puissance de feu augmentee !\n");
+            } else if (choix == 2 && joueur->ferraille >= 50) {
+                joueur->ferraille -= 50;
+                joueur->bouclierMax++;
+                printf("Generateur de bouclier ameliore !\n");
+            } else if (choix == 3 && joueur->ferraille >= 30) {
+                joueur->ferraille -= 30;
+                joueur->moteurs++; // Pense à l'ajouter dans ta struct !
+                printf("Moteurs pousses ! Esquive amelioree.\n");
+            }
         }
-        SLEEP_MS(1200); // Laisse le temps de lire
+        else if (categorie == 3) {
+             printf("\n[SERVICE] Aucune promotion disponible actuellement.\n");
+        }
+
+        SLEEP_MS(1000);
     }
 }
